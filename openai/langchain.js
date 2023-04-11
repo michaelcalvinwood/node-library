ec7bade291bc40a1c0bbe7a11db3d9e99a331621
Michael Wood <michaelwood33311@icloud.com>
2023-04-10 20:55:34 -0400

    added getting openai embedding

--- openai/langchain.js
+++ openai/langchain.js
@@ -1,12 +1,27 @@
-const llms = require ("langchain/llms");
-
+const { OpenAI} = require ("langchain/llms/openai");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config;
+
+
+const embedding = async (input) => {
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+      });
+      const openai = new OpenAIApi(configuration);
+      const embeddingResponse = await openai.createEmbedding({
+        model: 'text-embedding-ada-002',
+        input,
+      })
+  
+      const [{ embedding }] = embeddingResponse.data.data
+      return embedding;
+}
 
 const run = async () => {
 
     //Instantiante the OpenAI model 
     //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
-    const model = new llms.OpenAI({ temperature: 0.9 });
+    const model = new OpenAI({ temperature: 0.9 });
 
     //Calls out to the model's (OpenAI's) endpoint passing the prompt. This call returns a string
     const res = await model.call(
@@ -15,5 +30,5 @@ const run = async () => {
     console.log({ res });
 };
 
-run();
+embedding ('hello')
 
