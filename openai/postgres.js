8f6c7fbdd935cc36ae4af97f55cc375605233fbf
Michael Wood <michaelwood33311@icloud.com>
2023-04-12 09:55:28 -0400

    added running program after database has connected

--- openai/postgres.js
+++ openai/postgres.js
@@ -1,21 +1,41 @@
+require('dotenv').config();
 const { Pool } = require('node-postgres');
- 
-(async () => {
-  const pool = new Pool({
-    user: 'tester',
-    host: '206.189.233.106',
-    database: 'test',
-    password: '',
-    port: 5432
-  });
-  
-  const client = await pool.connect();
+
+const config = {
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432
+};
+const pool = new Pool(config);
+
+let connectedFlag = false;
+
+let pgClient;
+
+const run = async () => {
+  if (!connectedFlag) return;
   try {
-    const res = await client.query('SELECT * from posts');
-    console.log(res);
+        const res = await pgClient.query('SELECT * from posts');
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+// connect to postgres database
+(async () => {
+  pgClient = await pool.connect();
+  try {
+    // const res = await pgClient.query('SELECT * from posts');
+    // console.log(res);
+    connectedFlag = true;
   } catch (error) {
     console.log(error);
-  } finally {
-    client.release();
-  }
+  } 
 })().catch(console.error);
+
+// give program 2 seconds to make the postgres connection and then run
+setTimeout(run, 2000);
+
