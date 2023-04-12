6898f5bbdebf071bd539315187cba1e326e73828
Michael Wood <michaelwood33311@icloud.com>
2023-04-12 10:22:33 -0400

    added getting embedding from openAI and storing in posts table

--- openai/postgres.js
+++ openai/postgres.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
 const { Client } = require('pg');
 const pgvector = require('pgvector/pg');
 
@@ -13,24 +14,40 @@ const pgClient = new Client(config);
 
 let connectedFlag = false;
 
+const getEmbedding = async (input) => {
+  const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const embeddingResponse = await openai.createEmbedding({
+      model: 'text-embedding-ada-002',
+      input,
+    })
 
+    const [{ embedding }] = embeddingResponse.data.data
+    return embedding;
+}
 
 const run = async () => {
   if (!connectedFlag) return;
   try {
-        const res = await pgClient.query('SELECT * from posts');
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
+    const string = 'hello world';
+    const embedding = await getEmbedding (string);
+    const q = `INSERT INTO posts (body, embedding) VALUES ('${string}', '${[pgvector.toSql(embedding)]}')`;
+    console.log('query', q);
+
+    await pgClient.query(q);   
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // connect to postgres database
 (async () => {
   await pgClient.connect();
   try {
-    // const res = await pgClient.query('SELECT * from posts');
-    // console.log(res);
+    const res = await pgClient.query('SELECT * from posts');
+    console.log(res);
     await pgvector.registerType(pgClient);
     connectedFlag = true;
   } catch (error) {
