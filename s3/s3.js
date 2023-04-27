e387d8eeddddc169f0faf2065c810f3f372ca9aa
Michael Wood <michaelwood33311@icloud.com>
2023-04-27 07:50:14 -0400

    added uploading file to s3 bucket in s3 library

--- s3/s3.js
+++ s3/s3.js
@@ -7,6 +7,7 @@ const fsp = require('fs').promises;
 const mime = require('mime-types');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
+var path = require('path');
 
 const {S3_ENDPOINT, S3_ENDPOINT_DOMAIN, S3_REGION, S3_KEY, S3_SECRET, S3_BUCKET} = process.env;
 
@@ -18,3 +19,31 @@ exports.client = (S3_ENDPOINT, S3_ENDPOINT_DOMAIN, S3_REGION, S3_KEY, S3_SECRET,
         secretAccessKey: S3_SECRET
     }
 });
+
+exports.uploadFile = async (s3Client, filename, bucketFolder, bucketFileName) => {
+    const data = await fsp.readFile(filename);
+    let contentType = '';
+
+    const localFileName = path.basename(filename);
+
+    contentType = mime.lookup(localFileName);
+
+    if (!contentType) contentType = 'application/octet-stream';
+
+    const bucketParams = {
+        Bucket: process.env.S3_BUCKET,
+        Key: `${bucketFolder}/${bucketFileName}`,
+        Body: data,
+        ACL: 'public-read',
+        'Content-Type': contentType
+      };
+    
+      try {
+        const data = await s3Client.send(new PutObjectCommand(bucketParams));
+        const link = `https://${process.env.S3_BUCKET}.${process.env.S3_ENDPOINT_DOMAIN}/${bucketParams.Key}`;
+        return link;
+      } catch (err) {
+        console.log("Error", err);
+        return '';
+      }      
+};
