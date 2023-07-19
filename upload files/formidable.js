586757d277abb5df350579efc57b16ea31133557
Michael Wood <michaelwood33311@icloud.com>
2023-07-19 12:35:08 -0400

    added allowing 2GB file sizes when uploading files

--- upload files/formidable.js
+++ upload files/formidable.js
@@ -1,7 +1,7 @@
 const formidable = require('formidable');
 
 app.post('/fileUpload', (req, res) => {
-    var form = new formidable.IncomingForm();
+    var form = new formidable.IncomingForm({maxFileSize: 2000 * 1024 * 1024});
     form.parse(req, function (err, fields, data) {
         console.log('form data', data);
         if (err) return console.error(err);
@@ -11,7 +11,7 @@ app.post('/fileUpload', (req, res) => {
         if (!input) return res.status(400).json('no input')
         
         /*
-         * Process file here
+         * Process file input here
          */
         
         // remove file
